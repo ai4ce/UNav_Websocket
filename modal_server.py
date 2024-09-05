2a4ee5fb82ad8dfb96fe35c9fa87f9b01dab402c
Surendhar <76396808+surendhar-palanisamy@users.noreply.github.com>
2024-09-05 19:43:38 +0530

    chore: Update modal_server.py with dependencies and build commands for Eigen library and implicit_dist package

--- modal_server.py
+++ modal_server.py
@@ -11,6 +11,46 @@ image  = (
         "apt-get install -y pkg-config libhdf5-dev gcc libgl1-mesa-glx libglib2.0-0",
         "pip install --upgrade pip",
     )
+        .run_commands(
+         "apt-get update",
+        "apt-get install -y build-essential python3 python3-pip git cmake libeigen3-dev libgoogle-glog-dev libgflags-dev libatlas-base-dev libeigen3-dev libsuitesparse-dev pkg-config libhdf5-dev gcc libgl1-mesa-glx libglib2.0-0",
+        "apt-get remove -y libeigen3-dev",  # Remove the existing version of Eigen3
+    )
+    .run_commands(
+        "apt-get install -y  cmake libsuitesparse-dev libgoogle-glog-dev libgflags-dev libatlas-base-dev libeigen3-dev"
+    )
+   .run_commands(
+       "apt-get install -y libceres-dev"
+   )
+   .run_commands(
+       "git clone https://gitlab.com/libeigen/eigen.git eigen"
+   )
+   .workdir("/eigen")
+   .run_commands(
+       "git checkout 3.4",
+        "mkdir build",
+   )
+   .workdir("/eigen/build")
+   .run_commands(
+        "cmake ..",
+        "make",
+        "make install",
+   )
+   .workdir("/")
+   .run_commands("ls")
+    .run_commands(
+        "git clone https://github.com/cvg/implicit_dist.git implicit_dist",
+        )
+    .workdir("/implicit_dist")
+    .run_commands(
+        "ls",
+        "python3 -m venv .venv",
+        ". .venv/bin/activate",
+        "pip install .",
+        "pip freeze",
+    )
+    .workdir('/root')
+    .run_commands("ls")
     .pip_install_from_requirements("src/requirements.txt")
     .pip_install("kornia")
     .pip_install('pytorch_lightning')
