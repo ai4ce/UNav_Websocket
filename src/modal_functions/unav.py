3089e29b3f05e9be22958b0528c47bac5f8f70d2
Surendhar <76396808+surendhar-palanisamy@users.noreply.github.com>
2024-09-22 19:51:05 +0530

    Refactor UnavServer class to handle image localization using PyTorch

--- src/modal_functions/unav.py
+++ src/modal_functions/unav.py
@@ -25,10 +25,12 @@ class UnavServer:
             Handle localization request by processing the provided image and returning the pose.
         """
         # Use GPU if available, otherwise fallback to CPU
-        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        # self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        
+        # print(f"Device: {self.device}")
         
         # Move the model to the same device
-        server.coarse_locator.coarse_vpr.global_extractor.model.to(self.device)
+        # server.coarse_locator.device = self.device
 
         query_image_data = (
             base64.b64decode(query_image_base64.split(",")[1])
@@ -36,12 +38,15 @@ class UnavServer:
             else base64.b64decode(query_image_base64)
         )
         query_image = Image.open(io.BytesIO(query_image_data)).convert("RGB")
-        query_image_tensor = torch.from_numpy(np.array(query_image)).float().to(self.device)
 
-        # Convert tensor back to PIL Image before passing to input_transform
-        query_image_pil = Image.fromarray(query_image_tensor.cpu().numpy().astype(np.uint8))
+        # query_image = Image.open(io.BytesIO(query_image_data)).convert("RGB")
+        # query_image_tensor = torch.from_numpy(np.array(query_image)).float().to(self.device)
 
-        pose = server.handle_localization(frame=query_image_pil, session_id="test")
+        # # Convert tensor back to PIL Image before passing to input_transform
+        # query_image_pil = Image.fromarray(query_image_tensor.cpu().numpy().astype(np.uint8))
+
+        pose = server.handle_localization(frame=query_image, session_id="test")
+        print("Pose: ", pose)
         pounded_pose = [int(coord) for coord in pose] if pose else None
         print(pounded_pose)
         return "Image localized"
