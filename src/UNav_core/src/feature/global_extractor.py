9240e288f065237158deeea69d115b9d3629c74d
Surendhar <76396808+surendhar-palanisamy@users.noreply.github.com>
2024-09-22 19:49:38 +0530

    Refactor Global_Extractors class to handle GPU acceleration and model loading
    - Moved the NetVladFeatureExtractor model to the GPU if available
    - Loaded the MixVPR model state to the same device as the GPU if available
    - Set the Global_Extractors device attribute based on GPU availability

--- src/UNav_core/src/feature/global_extractor.py
+++ src/UNav_core/src/feature/global_extractor.py
@@ -6,23 +6,31 @@ import torch
 
 class Global_Extractors():
     def __init__(self, config):
-        self.root=config['IO_root']
+        self.root = config['IO_root']
         self.extractor = config['feature']['global']
+        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")  # Set device
 
     def netvlad(self, content):
-        return NetVladFeatureExtractor(join(self.root,content['ckpt_path']), arch=content['arch'],
-         num_clusters=content['num_clusters'],
-         pooling=content['pooling'], vladv2=content['vladv2'], nocuda=content['nocuda'])
-    
-    def mixvpr(self,content):
-        model = VPRModel(backbone_arch=content["backbone_arch"], 
-                        layers_to_crop=[content['layers_to_crop']],
-                        agg_arch=content['agg_arch'],
-                        agg_config=content['agg_config'],
-                        )
+        model = NetVladFeatureExtractor(join(self.root, content['ckpt_path']), 
+                                        arch=content['arch'],
+                                        num_clusters=content['num_clusters'],
+                                        pooling=content['pooling'], 
+                                        vladv2=content['vladv2'], 
+                                        nocuda=content['nocuda'])
+        if hasattr(model, 'model'):  # Check if it has a model attribute
+            model.model = model.model.to(self.device)  # Move model to the GPU
+         # Move NetVlad model to GPU if available
+        return model
 
-        state_dict = torch.load(content["ckpt_path"])
+    def mixvpr(self, content):
+        model = VPRModel(backbone_arch=content["backbone_arch"], 
+                         layers_to_crop=[content['layers_to_crop']],
+                         agg_arch=content['agg_arch'],
+                         agg_config=content['agg_config'],
+                         )
+        state_dict = torch.load(content["ckpt_path"], map_location=self.device)  # Load model state to the same device
         model.load_state_dict(state_dict)
+        model = model.to(self.device)  # Move MixVPR model to GPU if available
         model.eval()
         return model
 
