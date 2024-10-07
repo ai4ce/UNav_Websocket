49feb8a633c79c9b88d834719cf9ee4f6429fbf4
Surendhar <76396808+surendhar-palanisamy@users.noreply.github.com>
2024-10-07 12:14:03 +0530

    Decrease planner method execution time (#10)
    * Add LightGlue weights download and save functionality
    
    * Refactor UnavServer class to load server and handle localization and navigation
    
    * Refactor UnavServer class to load server and handle localization and navigation
    
    * Refactor UnavServer class to enable memory snapshot and adjust concurrency settings
    
    * Refactor UnavServer class to remove unused imports and commented out code
    
    * Refactor unav.py to remove unused imports and commented out code
    
    * Refactor unav.py to remove unused imports and commented out code
    
    * Refactor config.yaml to set load_all_maps to False

--- src/modal_functions/modal_config.py
+++ src/modal_functions/modal_config.py
@@ -4,6 +4,7 @@ from pathlib import Path
 volume = Volume.from_name("Visiondata")
 
 MODEL_URL = "https://download.pytorch.org/models/vgg16-397923af.pth"
+LIGHTGLUE_URL = "https://github.com/cvg/LightGlue/releases/download/v0.1_arxiv/superpoint_lightglue.pth"
 
 # Get the current file's directory
 current_dir = Path(__file__).resolve().parent
@@ -16,6 +17,9 @@ def download_torch_hub_weights():
     import torch
     model_weights = torch.hub.load_state_dict_from_url(MODEL_URL, progress=True)
     torch.save(model_weights, "vgg16_weights.pth")
+
+    lightglue_weights = torch.hub.load_state_dict_from_url(LIGHTGLUE_URL, progress=True)
+    torch.save(lightglue_weights,"superpoint_lightglue_v0-1_arxiv-pth")
 
 
 app = App(
--- src/modal_functions/unav.py
+++ src/modal_functions/unav.py
@@ -1,24 +1,37 @@
-from modal import method, gpu
-from typing import Optional, Dict
+from modal import method, gpu, build, enter
 
 from modal_config import app, unav_image, volume
 from logger_utils import setup_logger
 
 
-@app.cls(image=unav_image, volumes={"/root/UNav-IO": volume}, gpu=gpu.Any())
+@app.cls(
+    image=unav_image,
+    volumes={"/root/UNav-IO": volume},
+    gpu=gpu.Any(),
+    enable_memory_snapshot=True,
+    concurrency_limit=3,
+    allow_concurrent_inputs=3,
+)
 class UnavServer:
-    @method()
-    def get_destinations_list(self):
+    
+    @build()
+    @enter()
+    def load_server(self):
         from server_manager import Server
         from modules.config.settings import load_config
 
         config = load_config("config.yaml")
 
-        server = Server(logger=None, config=config)
+        self.server = Server(logger=setup_logger(), config=config)
 
-        response = server.get_destinations_list(building="LightHouse", floor="6_floor")
+    @method()
+    def get_destinations_list(self):
+
+        response = self.server.get_destinations_list(
+            building="LightHouse", floor="6_floor"
+        )
         return response
-
+    
     @method()
     def planner(
         self,
@@ -29,35 +42,29 @@ class UnavServer:
         place: str = "",
         base_64_image: str = None,
     ):
-
+    
         import json
-        from server_manager import Server
-        from modules.config.settings import load_config
-        from logger_utils import setup_logger
+        import time
         import base64
         import io
         from PIL import Image
-        from server_manager import Server
-        from modules.config.settings import load_config
-
-        config = load_config("config.yaml")
-
-        server = Server(logger=setup_logger(), config=config)
-
+    
         """
             Handle localization request by processing the provided image and returning the pose.
         """
-
+    
+        start_time = time.time()  # Start time for the entire function
+    
         query_image_data = (
             base64.b64decode(base_64_image.split(",")[1])
             if "," in base_64_image
             else base64.b64decode(base_64_image)
         )
         query_image = Image.open(io.BytesIO(query_image_data)).convert("RGB")
-
+    
         print("Query Image Converted from base64 to PIL Image")
-
-        response = server.select_destination(
+    
+        response = self.server.select_destination(
             session_id=session_id,
             place=place,
             building=building,
@@ -68,11 +75,26 @@ class UnavServer:
             print("Desintation Set to id: " + "07993")
         else:
             print(response)
-
-        pose = server.handle_localization(frame=query_image, session_id=session_id)
-
+    
+        # Measure time for handle_localization
+        start_localization_time = time.time()
+        pose = self.server.handle_localization(frame=query_image, session_id=session_id)
+        end_localization_time = time.time()
+        localization_time = end_localization_time - start_localization_time
+        print(f"Localization Time: {localization_time:.2f} seconds")
+    
         print("Pose: ", pose)
-
-        trajectory = server.handle_navigation(session_id)
-
+    
+        # Measure time for handle_navigation
+        start_navigation_time = time.time()
+        trajectory = self.server.handle_navigation(session_id)
+        end_navigation_time = time.time()
+        navigation_time = end_navigation_time - start_navigation_time
+        print(f"Navigation Time: {navigation_time:.2f} seconds")
+    
+        end_time = time.time()  # End time for the entire function
+        elapsed_time = end_time - start_time  # Calculate elapsed time for the entire function
+    
+        print(f"Total Execution Time: {elapsed_time:.2f} seconds")  # Print total elapsed time
+    
         return json.dumps({"trajectory": trajectory})
